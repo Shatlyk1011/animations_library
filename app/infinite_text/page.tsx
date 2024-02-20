8e8b4861ef1a47b4af2552795cb66883b2c6a428
Abdullayev Shatlyk <gj_wp@mail.ru>
2024-02-20 19:01:34 +0500

    add infinite text scroll animation

---
+++ app/infinite_text/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { FC, useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+interface Props {}
+
+import styles from "./styles.module.scss";
+import Image from "next/image";
+
+export default function Index() {
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  const slider = useRef(null);
+  let xPercent = 0;
+  let direction = -1;
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: 0,
+        end: window.innerHeight,
+        scrub: 0.25,
+        onUpdate: (e) => (direction = e.direction * -1),
+      },
+      x: "-=500px",
+    });
+    requestAnimationFrame(animation);
+  }, []);
+
+  const animation = () => {
+    if (xPercent < -100) {
+      xPercent = 0;
+    }
+
+    if (xPercent > 0) {
+      xPercent = -100;
+    }
+    gsap.set(firstText.current, { xPercent: xPercent });
+    gsap.set(secondText.current, { xPercent: xPercent });
+    requestAnimationFrame(animation);
+    xPercent += 0.1 * direction;
+  };
+  return (
+    <main className={styles.main}>
+      <Image src="/images/bg-1.jpg" fill={true} alt="bg" />
+
+      <div className={styles.sliderContainer}>
+        <div ref={slider} className={styles.slider}>
+          <p ref={firstText}>Lorem ipsum dolor sit amet.</p>
+          <p ref={secondText}>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+    </main>
+  );
+}
