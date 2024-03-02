8e5e9cc6c2cd58fc84f9405c5fdc597e24c3f4e2
Abdullayev Shatlyk <gj_wp@mail.ru>
2024-03-02 14:00:32 +0500

    change infinite text and mask text folders

--- app/Fancy/page.tsx
+++ app/Fancy/page.tsx
@@ -7,8 +7,6 @@ const Page = () => {
   return (
     <main className={styles.main}>
       <div className={styles.nav}>
-        <Link href="/Fancy/infinite_text">Infinite Text</Link>
-        <Link href="/Fancy/mask_text">Mask Text</Link>
         <Link href="/Fancy/svg_bezier_curve">Svg Bezier Curve</Link>
         <Link href="/Fancy/sticky_cursor">Sticky Cursor</Link>
         <Link href="/Fancy/spotlight">Spotlight</Link>
--- app/Text/page.tsx
+++ app/Text/page.tsx
@@ -7,6 +7,8 @@ const Page = () => {
   return (
     <main className={styles.main}>
       <div className={styles.nav}>
+        <Link href="/Text/infinite_text">Infinite Text</Link>
+        <Link href="/Text/mask_text">Mask Text</Link>
         <Link href="/Text/text_clip_mask_on_scroll">Text Clip Mask On Scroll</Link>
         <Link href="/Text/text_gradient_scroll">Text Gradient Scroll</Link>
         <Link href="/Text/text_mask_animation">Text Mask Animation</Link>
